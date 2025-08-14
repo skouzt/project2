1db3f883751d8c363814cfb8bc50beec32255ebf
skouzt <144224086+skouzt@users.noreply.github.com>
2025-08-14 22:10:51 +0530

    initial commit

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const companionsession = () => {
+  return (
+    <div>companionsession</div>
+  )
+}
+
+export default companionsession
---
+++ app/companions/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const newcompanion = () => {
+  return (
+    <div>newcompanion</div>
+  )
+}
+
+export default newcompanion
